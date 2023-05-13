a073e822fefcefd126d078964b7f85a8331d3177
shogle <seyoumoon@gmail.com>
2023-05-13 21:52:23 +0900

    modify adduser

--- create_container.sh
+++ create_container.sh
@@ -9,8 +9,8 @@ while read line || [ -n "$line" ] ; do
     echo "create $line 's Ubuntu Container"
     ((k=$i+1))
     ((j=$i+49))
-    docker run -it -d --env USERNAME=$line -h=$line --name $line -p $i:22 -p $k-$j:$k-$j beharbor_ubuntu:1.0
-    docker exec $line "/root/create_user.sh $line"
+    docker run -it -d --env USERNAME=$line -h=$line --name $line -p $i:22 -p $k-$j:$k-$j beharbor_ubuntu:1.1
+    docker exec -it $line "/root/create_user.sh" "$line"
     ufw allow $i:$j/tcp
     ufw allow $i:$j/udp
     ((i+=50))
